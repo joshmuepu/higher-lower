2699a7597f4b1e4df174ddcdfe7ddb47f2d5b20e
Josh Muepu <josh.muepu@swimlane.com>
2022-12-30 14:20:49 -0500

    updated the code to randomly choose account

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from art import logo, vs
 from game_data import data
+import random
 
 #print(logo)
 #print(data[0].get('name'))
@@ -7,40 +8,59 @@ from game_data import data
 #print(f"Compare A: ")
 count = 0
 
-for star in range(0, len(data)-1):
-    name = data[star]['name']
-    follower_count = data[star]['follower_count']
-    description = data[star]['description']
-    country = data[star]["country"]
-    print(logo)
+account_a = random.choice(data)
+account_b = random.choice(data)
+if account_a == account_b:
+  account_b = random.choice(data)
+  
+name_a = account_a['name']
+follower_count_a = account_a['follower_count']
+description_a = account_a['description']
+country_a = account_a["country"]
+print(logo)
 
-    print(f"Compare A: {name}, a {description} from {country}")
-    next_name = data[star + 1]['name']
-    next_follower_count = data[star + 1]['follower_count']
-    next_description = data[star + 1]['description']
-    next_country = data[star + 1]["country"]
+should_continue = True
 
-    print(vs)
-    print(f"Against B: {next_name}, a {next_description} from {next_country}")
+while(should_continue):
 
-    user_choice = input("Who has more followers? Type 'A' or 'B': ")
-
-    if user_choice == "A":
-        if follower_count > next_follower_count:
-            count += 1
-            print(f"You're right. Current score: {count}.")
-
-        else:
-            print(f"you lost on count: {count}.")
-            break
-
-    elif user_choice == "B":
-        if next_follower_count > follower_count:
-            count += 1
-            print(f"You're right. Current score: {count}. ")
-        else:
-            print(f"You lost on count {count}.")
-            break
-    else:
-        print("Please, enter valid entry.")
-        break
+  print(f"Compare A: {name_a}, a {description_a} from {country_a}")
+  
+  name_b = account_b['name']
+  follower_count_b = account_b['follower_count']
+  description_b = account_b['description']
+  country_b = account_b["country"]
+  
+  print(vs)
+  print(f"Against B: {name_b}, a {description_b} from {country_b}")
+  
+  user_choice = input("Who has more followers? Type 'A' or 'B': ")
+  
+  if user_choice == "A":
+      if follower_count_a > follower_count_b:
+          count += 1
+          print(f"You're right. Current score: {count}.")
+          account_b = random.choice(data)
+          if account_a == account_b:
+            account_b = random.choice(data)
+          
+      else:
+          print(f"you lost on count: {count}.")
+          should_continue = False
+            
+  elif user_choice == "B":
+      if follower_count_b > follower_count_a:
+          count += 1
+          print(f"You're right. Current score: {count}. ")
+          name_a = name_b
+          description_a = description_b
+          country_a = country_b
+          account_b = random.choice(data)
+          if account_b == account_a:
+            account_b = random.choice(data)
+      else:
+          print(f"You lost on count {count}.")
+          should_continue = False
+          
+  else:
+      print("Please, enter valid entry.")
+      should_continue = False
